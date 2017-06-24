dc160be299bc63be36e72ca4f4e25f977f007f42
Ivan Kovtunov <qovtunov@gmail.com>
2017-06-24 08:51:06 +0300

    improvements

--- src/test/java/WebTests/FeaturedMarqueTest.java
+++ src/test/java/WebTests/FeaturedMarqueTest.java
@@ -21,23 +21,25 @@ public class FeaturedMarqueTest extends MainTestWeb{
     public FeaturedMarque featuredMarque;
 
     @Test
-    public void Large1200_1599px() {  //please rename the test
+    public void Large1200_1599px() {
         featuredMarque = new FeaturedMarque(driver);
 
-        setSize(1200, 768); //set size
-        Assert.assertTrue(isElementVisible(featuredMarque.banner)); //checking that banner appeared
+        setSize(1200, 768);
+        Assert.assertTrue(isElementVisible(featuredMarque.banner)); //component exists or not
 
-        //please insert here screenshot test
+        // screenshot assert here
 
         hoverOverElement(featuredMarque.playButton);
-        //need an assert
+        Assert.assertEquals(featuredMarque.playButton.getCssValue("opacity"), "0.6"); // opacity check
 
         clickElement(featuredMarque.playButton);
-        Assert.assertTrue(isElementVisible(featuredMarque.videoContainer)); // video test
+        Assert.assertTrue(isElementVisible(featuredMarque.videoContainer)); // open player
+
         clickElement(featuredMarque.videoContainerCloseButton);
+        Assert.assertTrue(isElementNotVisible(featuredMarque.videoContainer)); // close player
 
         clickElement(featuredMarque.downButton);
-        Assert.assertEquals(featuredMarque.downButton.getAttribute("href"), ConfigProperties.getProperty("baseUrl")+'#'); //down arrow test
+        Assert.assertEquals(featuredMarque.downButton.getAttribute("href"), ConfigProperties.getProperty("baseUrl")+'#'); //down arrow check
 
 
         //NEXT BREAKPOINT:
@@ -45,38 +47,42 @@ public class FeaturedMarqueTest extends MainTestWeb{
         /*setSize(992, 768); //set size
         Assert.assertTrue(isElementVisible(featuredMarque.banner)); //checking that banner appeared
 
-        //please insert here screenshot test
+        // screenshot assert here
 
         hoverOverElement(featuredMarque.playButton);
-        //need an assert
+        Assert.assertEquals(featuredMarque.playButton.getCssValue("opacity"), "0.6"); // opacity check
 
         clickElement(featuredMarque.playButton);
-        Assert.assertTrue(isElementVisible(featuredMarque.videoContainer)); // video test
+        Assert.assertTrue(isElementVisible(featuredMarque.videoContainer)); // open player
+
         clickElement(featuredMarque.videoContainerCloseButton);
+        Assert.assertTrue(isElementNotVisible(featuredMarque.videoContainer)); // close player
 
         clickElement(featuredMarque.downButton);
-        Assert.assertEquals(featuredMarque.downButton.getAttribute("href"), ConfigProperties.getProperty("baseUrl")+'#'); //down arrow test*/
+        Assert.assertEquals(featuredMarque.downButton.getAttribute("href"), ConfigProperties.getProperty("baseUrl")+'#'); //down arrow check*/
 
     }
 
     @Test
-    public void Medium992_1199px() {  //please rename the test
+    public void Medium992_1199px() {
         featuredMarque = new FeaturedMarque(driver);
 
         setSize(992, 768); //set size
-        Assert.assertTrue(isElementVisible(featuredMarque.banner)); //checking that banner appeared
+        Assert.assertTrue(isElementVisible(featuredMarque.banner)); //component exists or not
 
-        //please insert here screenshot test
+        // screenshot assert here
 
         hoverOverElement(featuredMarque.playButton);
-        //need an assert
+        Assert.assertEquals(featuredMarque.playButton.getCssValue("opacity"), "0.6"); // opacity check
 
         clickElement(featuredMarque.playButton);
-        Assert.assertTrue(isElementVisible(featuredMarque.videoContainer)); // video test
+        Assert.assertTrue(isElementVisible(featuredMarque.videoContainer)); // open player
+
         clickElement(featuredMarque.videoContainerCloseButton);
+        Assert.assertTrue(isElementNotVisible(featuredMarque.videoContainer)); // close player
 
         clickElement(featuredMarque.downButton);
-        Assert.assertEquals(featuredMarque.downButton.getAttribute("href"), ConfigProperties.getProperty("baseUrl")+'#'); //down arrow test
+        Assert.assertEquals(featuredMarque.downButton.getAttribute("href"), ConfigProperties.getProperty("baseUrl")+'#'); //down arrow check
 
     }
 
@@ -92,13 +98,21 @@ public class FeaturedMarqueTest extends MainTestWeb{
     }
 
     @Test
-    public void compareScreenshots() throws IOException {
+    public void compareScreenshots() throws IOException, InterruptedException {
 
         WebElement logoImage = driver.findElement(By.cssSelector("#content > div > div > div.featuredMarqueeComponent.parbase.section"));
 
+
+
         BufferedImage expectedImage = ImageIO.read(new File(System.getProperty("user.dir") +"/Screenshots/ElementScreenshot.png"));
         Screenshot logoImageScreenshot = new AShot().takeScreenshot(driver, logoImage);
+        ImageIO.write(logoImageScreenshot.getImage(),"PNG",new File(System.getProperty("user.dir") +"/Screenshots/ElementScreenshotActual.png"));
+
+
         BufferedImage actualImage = logoImageScreenshot.getImage();
+
+
+        Thread.sleep(3000);
 
         ImageDiffer imgDiff = new ImageDiffer();
         ImageDiff diff = imgDiff.makeDiff(actualImage, expectedImage);
